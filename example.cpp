5a425dc683c2a160eb35e3ac941b0aa336056f02
Arne Döring <arne.doering@gmx.net>
2017-08-16 12:55:16 +0200

    initial commit

---
+++ example.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <glm/glm.hpp>
+#include <GL/glew.h>
+
+
+std::vector<uint16_t> indices;
+std::vector<glm::vec4> positions;
+std::vector<glm::vec4> normals;
+
+enum BufferNames {
+  Buffer_Indices,
+  Buffer_Positions,
+  Buffer_Normals,
+  Buffer_Count
+};
+
+void initializeBuffers() {
+  uint32_t buffers[Buffer_Count]; // handles for buffers
+  glCreateBuffers(Buffer_Count, buffers);
+  glNamedBufferData(buffers[Buffer_Indices],   indices.size() * sizeof(uint16_t), indices.data(), 0);
+  glNamedBufferData(buffers[Buffer_Positions], positions.size() * sizeof(glm::vec4), positions.data(), 0);
+  glNamedBufferData(buffers[Buffer_Normals],   normals.size() * sizeof(glm::vec4), normals.data(), 0);
+}
+
+
+template <typename T>
+struct Buffer {
+  uint32_t handle;
+
+  Buffer(T* data, ssize_t N) {
+    glCreateBuffers(1, &handle);
+    if( N > 0 ) {
+      glNamedBufferData(handle, N * sizeof(T), data, 0);
+    }
+  }
+
+  Buffer(std::vector<T> data) :
+};
